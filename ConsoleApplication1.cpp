4c89ecc9cd123a6fb422c0a3d53ea2a6fef4b686
gremlin079 <sashdmitriev2006@gmail.com>
2025-03-16 11:36:16 +0000

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,193 +1,198 @@
 ﻿#include <iostream>
-#include <vector>
-#include <map>
+#include <cstdlib>
+#include <ctime>
 #include <string>
-#include <fstream>
-#include "WorkCsv.hpp"
+#include <vector>
 #include <sstream>
-#include <filesystem>
-#include <cstdio>
-#include <locale>
-#include <codecvt>
+#include <algorithm>
 
-Seminar::Seminar(std::string name)
+
+class Expression
 {
-    this->name = name;
+protected:
+	std::string rawExpression;
+	std::string normolizedExpression;
+	std::string type;
+
+public:
+	Expression(std::string& rawExpression, std::string& type)
+	{
+		this->rawExpression = rawExpression;
+		this->type = type;
+	}
+
+	std::string getRawExpression()
+	{
+		return rawExpression;
+	}
+	
+	std::string getNormolizedExpression()
+	{
+		return normolizedExpression;
+	}
+
+	std::string getType()
+	{
+		return type;
+	}
+
+	void setNormolizedExpression(std::string & normolizedExpression)
+	{
+		this->normolizedExpression = normolizedExpression;
+	}
+
+	void normalaize();
+};
+
+class ArithmeticExpressionProcessor
+{
+	Expression& expr;
+
+public:
+	ArithmeticExpressionProcessor(Expression &expr) : expr(expr)
+	{
+	}
+	
+	void convertToStandartForm()
+	{
+		int colvo_x = 0;
+		std::vector<int> coefficents;
+		int i = 0;
+		std::vector<int> poz_znak;
+		std::vector<int> poz_scobka;
+		std::vector<int> x_coeff {1};
+
+
+		for (char x : expr.getRawExpression())
+		{
+			if (x == 'x')
+			{
+				colvo_x += 1;
+			}
+
+			if (x == '+' or x == '-')
+			{
+				poz_znak.push_back(i);
+			}
+			
+			if (x == ')')
+			{
+				poz_scobka.push_back(i);
+			}
+			i += 1;
+		}
+		
+		for (int i = 0; i < colvo_x; i++)
+		{
+			std::string Help_string;
+			for (int k = poz_znak[i]; k < poz_scobka[i]; k++)
+			{
+				Help_string += expr.getRawExpression()[k];
+			}
+			
+			coefficents.push_back(std::stoi(Help_string));
+		}
+
+		for (int i = 1; i < colvo_x; i++)
+		{
+			int n = coefficents.size();
+			std::vector<bool> mask(n, false);
+			int total_sum = 0;
+
+			std::fill(mask.end() - i, mask.end(), true);
+
+			do {
+				int product = 1;
+				for (int k = 0; k < n; ++k)
+				{
+					if (mask[k])
+					{
+						product *= coefficents[k];
+					}
+				}
+				
+				total_sum += product;
+			} while (std::next_permutation(mask.begin(), mask.end()));
+			
+			x_coeff.push_back(total_sum);
+		}
+		std::string return_this;
+		for (int i = 0; i < colvo_x; i++)
+		{
+			if (i == 0)
+			{
+				return_this += "x^" + std::to_string(colvo_x);
+			}
+			else
+			{
+				if (x_coeff[i] >= 0)
+				{
+					return_this += " + " + std::to_string(x_coeff[i]) + "x^" + std::to_string(colvo_x - i);
+				}
+				else
+				{
+					return_this += " - " + std::to_string(abs(x_coeff[i])) + "x^" + std::to_string(colvo_x - i);
+				}
+			}
+		
+		}
+		
+		int free_coef = 1;
+		for (int el : coefficents)
+		{
+			free_coef *= el;
+		}
+		
+		if (free_coef >= 0)
+		{
+			return_this += " + " + std::to_string(free_coef);
+		}
+		else
+		{
+			return_this += " - " + std::to_string(abs(free_coef));
+		}
+
+		expr.setNormolizedExpression(return_this);
+	
 }
 
-CSVHandler::CSVHandler(std::string path)
-{
-    this->path = path;
-    std::vector<Seminar> a = {};
-    this->seminars = a;
-}
+	//void addConstant()
+	//{
+	//	
+	//}
+};
 
-std::vector<Seminar> CSVHandler::getSeminars()
-{
-    return seminars;
-}
+//class LogicalExpressionProcessor : public Expression
+//{
+//public:
+//	LogicalExpressionProcessor(std::string& rawExpression, std::string& type) : Expression(rawExpression, type)
+//	{
+//	}
+//	
+//	void convertToStandartForm()
+//	{
+//	}
+//
+//	void addConstant()
+//	{
+//	}
+//};
 
-
-void CSVHandler::readData()
-{
-    //std::setlocale(LC_ALL, "ru_RU-UTF-8 BOM");
-    std::vector<Seminar> seminars;
-    for (const auto& entry : std::filesystem::directory_iterator(path))
-    {
-        if (entry.is_regular_file())
-        {
-            std::ifstream file(entry.path());
-            
-            std::vector<std::string> dates;
-            std::vector<Student> students;
-            std::string line;
-            int counter = 0;
-            std::string name;
-            int f = 0;
-            std::string filename = entry.path().filename().string();
-            while (filename[f] != '.')
-            {
-                name += filename[f];
-                f += 1;
-                if (f >= 50)
-                {
-                    break;
-                }
-            }
-            while (std::getline(file, line))
-            {
-                std::stringstream ss(line);
-                std::string block;
-                int counter2 = 0;
-                Student newobj;
-                std::map<std::string, short> MarksForRead;
-                std::vector<short> HelpMarks;
-                std::string NameForRead;
-                while (std::getline(ss, block, ';'))
-                {
-                    if (counter == 0)
-                    {
-                        if (block[0] == 'И')
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            dates.push_back(block);
-                        }
-                    }
-                    else
-                    {
-                        if (counter2 == 0)
-                        {
-                            NameForRead = block;
-   
-                        }
-                        else
-                        {
-                            if (block[0] == 'Н')
-                            {
-                                HelpMarks.push_back(-1);
-                            }
-                            if (block[0] == 'П')
-                            {
-                                HelpMarks.push_back(0);
-                            }
-                            if (block[0] == 'Р')
-                            {
-                                HelpMarks.push_back(1);
-                            }
-                        }
-                    }
-                    counter2 += 1;
-                }
-                if (counter != 0)
-                {
-                    newobj.name = NameForRead;
-                    for (int i = 0; i < dates.size(); i++)
-                    {
-                        MarksForRead[dates[i]] = HelpMarks[i];
-                    }
-                    newobj.marks = MarksForRead;
-                    students.push_back(newobj);
-                }
-
-                counter += 1;
-            }
-
-            Seminar a(name);
-            a.dates = dates;
-            a.students = students;
-            seminars.push_back(a);
-            file.close();
-        }
+void Expression::normalaize() {
+    if (this->type == "Арифметическое") {
+        ArithmeticExpressionProcessor processor(*this);
+        processor.convertToStandartForm();
     }
-    this->seminars = seminars;
 }
 
-
-
-void CSVHandler::writeData(const std::vector<Seminar>& a)
-{
-    int i = 0;
-    std::vector<std::string> HelpDates = a[0].dates;
-    for (const auto& entry : std::filesystem::directory_iterator(path))
-    {
-        if (entry.is_regular_file())
-        {
-            std::ofstream file(entry.path(), std::ios::trunc);
-            std::string oldName = path + "\\" + seminars[i].name + ".csv";
-            std::string newName = path + "\\" + a[i].name + ".csv";
-            
-            
-            /*rename(oldName.c_str(), newName.c_str());*/
-            file << "Имя студента;";
-            for (int j = 0; j < a[0].dates.size(); j++)
-            {
-                file << a[0].dates[j] << ";";
-            }
-            file << std::endl;
-            for (int j = 0; j < a[i].students.size(); j++)
-            {
-                file << a[i].students[j].name << ";";
-                for (int k = 0; k < HelpDates.size(); k++)
-                {
-                    if (a[i].students[j].marks.at(HelpDates[k]) == -1)
-                    {
-                        file << "Н;";
-                    }
-                    if (a[i].students[j].marks.at(HelpDates[k]) == 0)
-                    {
-                        file << "П;";
-                    }
-                    if (a[i].students[j].marks.at(HelpDates[k]) == 1)
-                    {
-                        file << "Р;";
-                    }
-                }
-				file << std::endl;
-            }
-			file.close();
-            if (rename(oldName.c_str(), newName.c_str()) != 0)
-            {
-                perror("Error renaming file");
-            }
-        
-        }
-        i += 1;
-    }
-    this->seminars = a;
-}
-
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Russian");
-    CSVHandler a("C:\\Users\\Александр\\Documents\\жопа");
-	a.readData();
-	std::vector<Seminar> b = a.getSeminars();
-    b[0].name = "test";
-    a.writeData(b);
+    
+    std::string a = "(x+5)(x-4)(x+7)(x-8)(x+9)";
+    std::string t = "Арифметическое";
+    Expression c(a, t);
+    c.normalaize();
+    std::cout << "Стандартный вид: " << c.getNormolizedExpression() << std::endl;
 
     return 0;
 }
-
